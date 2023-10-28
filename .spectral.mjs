00d86029708b5d2a91b870c0f4170543f3f866da
Kristi <kbierut@uw.edu>
2023-10-27 18:19:23 -0700

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIyNDU3OQ/spectral.js?branch_slug=main&token=13311e90-a468-428b-92b4-2c09255fa461";
+export default { extends: ruleset };
